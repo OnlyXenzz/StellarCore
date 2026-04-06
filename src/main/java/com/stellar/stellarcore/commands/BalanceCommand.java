ab69548ab4d914828deb7c235249381eee6865c3
StellarProject <rifkydanangrizqullah@gmail.com>
2026-04-06 10:18:23 +0700

    Update BalanceCommand.java

--- src/main/java/com/stellar/stellarcore/commands/BalanceCommand.java
+++ src/main/java/com/stellar/stellarcore/commands/BalanceCommand.java
@@ -2,41 +2,18 @@ package com.stellar.stellarcore.commands;
 
 import com.stellar.stellarcore.StellarCore;
 import com.stellar.stellarcore.utils.TextUtils;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class BalanceCommand implements CommandExecutor {
-    
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        Player target;
-        
-        if (args.length > 0) {
-            target = Bukkit.getPlayer(args[0]);
-            if (target == null) {
-                sender.sendMessage(TextUtils.getPrefix() + TextUtils.format("&ᴄᴘʟᴀʏᴇʀ ɴᴏᴛ ꜰᴏᴜɴᴅ!"));
-                return true;
-            }
-        } else {
-            if (!(sender instanceof Player)) {
-                sender.sendMessage(TextUtils.getPrefix() + TextUtils.format("&ᴄᴘʟᴇᴀꜱᴇ ꜱᴘᴇᴄɪꜰʏ ᴀ ᴘʟᴀʏᴇʀ!"));
-                return true;
-            }
-            target = (Player) sender;
-        }
-        
-        double balance = StellarCore.getInstance().getEconomyManager().getBalance(target);
-        String formatted = StellarCore.getInstance().getEconomyManager().formatCurrency(balance);
-        
-        if (target == sender) {
-            sender.sendMessage(TextUtils.getPrefix() + TextUtils.format("&ᴀʏᴏᴜʀ ʙᴀʟᴀɴᴄᴇ: " + formatted));
-        } else {
-            sender.sendMessage(TextUtils.getPrefix() + TextUtils.format("&ʙ" + target.getName() + " &ᴀʙᴀʟᴀɴᴄᴇ: " + formatted));
-        }
-        
+    public boolean onCommand(CommandSender s, Command c, String l, String[] a) {
+        if (!(s instanceof Player)) return true;
+        double bal = StellarCore.getInstance().getEconomyManager().getBalance((Player) s);
+        s.sendMessage(TextUtils.getPrefix() + TextUtils.format("&ᴀʙᴀʟᴀɴᴄᴇ: " + 
+            StellarCore.getInstance().getEconomyManager().formatCurrency(bal)));
         return true;
     }
 }
